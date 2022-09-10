1655a4039467365c895d0bc9ec20a84282561cfb
ream <ream_tp@outlook.com>
2022-09-10 13:21:30 -0500

    Agregado campo para ordenar categorias

--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -5,14 +5,15 @@ const router = Router()
 
 router.get('/', async (req, res) => {
   const { id } = req.query
+  const orderBy = (req.query.order && (req.query.order === 'asc' || req.query.order === 'desc')) ? req.query.order : 'asc'
 
   if (id !== null && id !== undefined) {
-    const response = await getCategoryById(id as string)
+    const response = await getCategoryById(id as string, orderBy)
     console.log(response)
     return res.send(response)
   }
 
-  const response = await getAllCategories()
+  const response = await getAllCategories(orderBy)
 
   return res.send(response)
 })
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -18,7 +18,7 @@ router.get('/search', async (req, res) => {
   const search = req.query.value ? String(req.query.value) : ''
   const limit = req.query.limit ? Number(req.query.limit) : 50
   const offset = (((Number(req.query.page) > 1) && req.query.page) ? Number(req.query.page) - 1 : 0) * 10
-  const orderBy = 'asc'
+  const orderBy = (req.query.order && (req.query.order === 'asc' || req.query.order === 'desc')) ? req.query.order : 'asc'
 
   const response = await searchProduct(search, orderBy, limit, offset)
 
